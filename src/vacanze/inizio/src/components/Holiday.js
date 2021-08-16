1c7ffea395fba46910f393fea1c0daa9d7f00bc5
merlinoalbus <roberto.mirabella84@gmail.com>
2021-08-16 19:15:37 +0200

    Progetto 3

--- src/vacanze/inizio/src/components/Holiday.js
+++ src/vacanze/inizio/src/components/Holiday.js
@@ -3,47 +3,56 @@ import axios from "axios";
 import SingleHoliday from "./SingleHoliday";
 const url = "https://react-corso-api.netlify.app/.netlify/functions/holiday";
 const Holiday = () => {
-  const [data, setData]=useState([]);
-  const [selectedItem, setSelectedItem]=useState(1);
-  const getData= async () =>{
-    try{
-    const response= await axios.get(url);
-    console.log(response);
-    setData(response.data);}
-    catch(error){
+  const [data, setData] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(1);
+  const getData = async () => {
+    try {
+      const response = await axios.get(url);
+      console.log(response);
+      setData(response.data);
+    } catch (error) {
       console.error(error);
     }
-  }
-  const getNextVacanza=()=> {
-      setSelectedItem((prevValue)=>{
-        if(prevValue+1===data.data.length) return 0
-        return prevValue+1
-      }
-    )
-  }
-  const getPrevVacanza=()=> {
-    setSelectedItem((prevValue)=>{
-      if(prevValue-1<0) return data.data.length-1
-      return prevValue-1
-    }
-  )
-}
-  useEffect(()=>{
+  };
+  const getNextVacanza = () => {
+    setSelectedItem((prevValue) => {
+      // @ts-ignore
+      if (prevValue + 1 === data.data.length) return 0;
+      return prevValue + 1;
+    });
+  };
+  const getPrevVacanza = () => {
+    setSelectedItem((prevValue) => {
+      // @ts-ignore
+      if (prevValue - 1 < 0) return data.data.length - 1;
+      return prevValue - 1;
+    });
+  };
+  useEffect(() => {
     getData();
-  },[])
-  if(data.success){
-    return <>
-    { data.data.length>0 ?(
-      <SingleHoliday {...data.data[selectedItem]}
-      next={getNextVacanza}
-      prev={getPrevVacanza}
-      />
-      ):(<h4>Nessuna Vacanza Trovata</h4>)
-    }
-    </>
-  }
-  else{
-    return <h3>Loading...</h3>
+  }, []);
+  // @ts-ignore
+  if (data.success) {
+    return (
+      <>
+        {
+          // @ts-ignore
+          data.data.length > 0 ? (
+            // @ts-ignore
+            <SingleHoliday
+              // @ts-ignore
+              {...data.data[selectedItem]}
+              next={getNextVacanza}
+              prev={getPrevVacanza}
+            />
+          ) : (
+            <h4>Nessuna Vacanza Trovata</h4>
+          )
+        }
+      </>
+    );
+  } else {
+    return <h3>Loading...</h3>;
   }
 };
 
